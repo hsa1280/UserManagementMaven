d5edc86575502c86a52dbb090cd44c11a0b35888
Shian <shianhuang2008@gmail.com>
2015-08-28 22:47:19 -0500

    Moved application.properties resources folder

--- src/main/java/com/shian/usermanamement/maven/config/DatasourceConfig.java
+++ src/main/java/com/shian/usermanamement/maven/config/DatasourceConfig.java
@@ -4,6 +4,7 @@ import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
@@ -11,17 +12,18 @@ import javax.sql.DataSource;
  * Created by shian_mac on 8/19/15.
  */
 @Configuration
+@PropertySource("classpath:application.properties")
 public class DatasourceConfig {
 
-    private String url = "jdbc:mysql://localhost:3306/mysql";
-    private String userName = "root";
-    private String passWord = "root";
-//    @Value( "${db.url}" )
-//    private String url;
-//    @Value("${db.userName}")
-//    private String userName;
-//    @Value("${db.passWord}")
-//    private String passWord;
+//    private String url = "jdbc:mysql://localhost:3306/mysql";
+//    private String userName = "root";
+//    private String passWord = "root";
+    @Value( "${db.url}" )
+    private String url;
+    @Value("${db.userName}")
+    private String userName;
+    @Value("${db.passWord}")
+    private String passWord;
 
 //	@Bean
 //	public PoolDataSourceBeanPostProcessor poolDataSourceBeanPostProcessor() {
