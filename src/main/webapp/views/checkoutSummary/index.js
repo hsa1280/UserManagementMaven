72b50f5a400ef17bbc1db19354e78dcccef2811b
Shian <shianhuang2008@gmail.com>
2015-09-12 15:35:01 -0500

    fixed typo

--- src/main/webapp/views/checkoutSummary/index.js
+++ src/main/webapp/views/checkoutSummary/index.js
@@ -2,10 +2,10 @@
  * Created by shian_mac on 9/2/15.
  */
 import angular from 'angular';
-import CheckoutControoler from './checkoutController';
+import CheckoutController from './checkoutController';
 import checkoutSummaryStateProvider from './checckoutSummaryStateProvider';
 
 export default angular.
     module('checkoutModule', []).
     config(checkoutSummaryStateProvider).
-    controller('CheckoutControoler', CheckoutControoler);
+    controller('CheckoutController', CheckoutController);
