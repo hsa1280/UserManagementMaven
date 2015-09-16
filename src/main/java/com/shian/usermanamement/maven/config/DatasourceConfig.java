14e4522604c699dec39118c6cccf0a66a2008393
Shian <shianhuang2008@gmail.com>
2015-09-15 20:25:20 -0500

    Moved application.propertites to META-INF, and moved code from application.yml to application.properties

--- src/main/java/com/shian/usermanamement/maven/config/DatasourceConfig.java
+++ src/main/java/com/shian/usermanamement/maven/config/DatasourceConfig.java
@@ -12,18 +12,18 @@ import javax.sql.DataSource;
  * Created by shian_mac on 8/19/15.
  */
 @Configuration
-@PropertySource("classpath:application.properties")
+@PropertySource("classpath:META-INF/application.properties")
 public class DatasourceConfig {
 
-    private String url = "jdbc:mysql://localhost:3306/mysql";
-    private String userName = "root";
-    private String passWord = "root";
-//    @Value( "${db.url}" )
-//    private String url;
-//    @Value("${db.userName}")
-//    private String userName;
-//    @Value("${db.passWord}")
-//    private String passWord;
+//    private String url = "jdbc:mysql://localhost:3306/mysql";
+//    private String userName = "root";
+//    private String passWord = "root";
+    @Value( "${db.url}" )
+    private String url;
+    @Value("${db.userName}")
+    private String userName;
+    @Value("${db.passWord}")
+    private String passWord;
 
     @Bean
     public DataSource dataSource() throws Exception {
